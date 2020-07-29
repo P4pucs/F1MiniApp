829e4893fead9eea1e82d569b7cf77e7b3e4656f
P4pucs <kovacs.kevin98@gmail.com>
2020-07-29 16:50:45 +0200

    added class for reading the seasons

--- app/src/main/java/com/kevin/f1miniapp/MainActivity.java
+++ app/src/main/java/com/kevin/f1miniapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.kevin.f1miniapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+
+    private Seasons seasons = new Seasons();;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(seasons.jsonParse());
+
+        requestQueue.start();
+        seasons.readSeason();
+
+
     }
 }
---
+++ app/src/main/java/com/kevin/f1miniapp/Seasons.java
@@ -0,0 +1,97 @@
+package com.kevin.f1miniapp;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Seasons {
+    private final String url = "http://ergast.com/api/f1/seasons.json";
+
+    private ArrayList<Season> seasons;
+
+    public Seasons() {
+        seasons = new ArrayList<>();
+
+    }
+
+    private class Season {
+        int date;
+        String wikiUrl;
+
+        private Season(int date, String url) {
+            this.date = date;
+            this.wikiUrl = url;
+        }
+
+        public int getDate() {
+            return date;
+        }
+
+        public String getWikiUrl() {
+            return wikiUrl;
+        }
+    }
+
+    public JsonObjectRequest jsonParse() {
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject MRData = response.getJSONObject("MRData");
+                            JSONObject seasonTable = MRData.getJSONObject("SeasonTable");
+
+                            JSONArray seasonsArray =  seasonTable.getJSONArray("Seasons");
+
+                            for(int i=0;i<seasonsArray.length();i++) {
+                                JSONObject season = seasonsArray.getJSONObject(i);
+                                int year = Integer.parseInt(season.getString("season"));
+                                String url = season.getString("url");
+                                seasons.add(new Season(year, url));
+                            }
+                            readSeason();
+
+                        } catch (JSONException e) {
+                            System.out.println("baj van");
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+        return request;
+    }
+
+    public void readSeason() {
+        for (int i=0;i<seasons.size();i++) {
+            System.out.println("year: " + seasons.get(i).date + " url: " + seasons.get(i).wikiUrl);
+        }
+    }
+
+    public ArrayList<Season> getList() {
+        return seasons;
+    }
+
+
+}
